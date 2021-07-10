26e66308e068802ef5dd49b0f6ba2a93811b57f3
johannaly <johanna@lyytinen.org>
2021-07-10 18:27:18 +0300

    9.10 Patientor backend step 3

---
+++ patientor_backend/src/services/diagnosisService.ts
@@ -0,0 +1,10 @@
+import diagnosesData from '../../data/diagnoses.json';
+import { Diagnoses } from '../types';
+
+const diagnoses: Array<Diagnoses> = diagnosesData;
+
+const getDiagnoses = (): Array<Diagnoses> => {
+    return diagnoses;
+};
+
+export default { getDiagnoses };
